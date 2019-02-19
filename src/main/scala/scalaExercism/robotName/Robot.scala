d542c6c0936cf10001a2e4b8682263f5bdb76460
reomor <reomor@gmail.com>
2019-02-19 10:18:04 +0300

    task002 robot-name optional

--- src/main/scala/scalaExercism/robotName/Robot.scala
+++ src/main/scala/scalaExercism/robotName/Robot.scala
@@ -1,16 +1,44 @@
 package scalaExercism.robotName
 
+import scala.collection.mutable
 import scala.util.Random
 
 class Robot() {
-  var name: String = generateName
-  def generateName: String = {
+  var name: String = generateUniqueName
+  def generateName: (String, String) = {
     val random = new Random()
-    Random.alphanumeric.filter(_.isLetter).take(2).mkString.toUpperCase +
-      (for(i <- 1 to 3) yield random.nextInt(10)).mkString
+    val nameLetters = Random.alphanumeric.filter(_.isLetter).take(2).mkString.toUpperCase
+    val nameDigits = (for (i <- 1 to 3) yield random.nextInt(10)).mkString
+    nameLetters -> nameDigits
   }
+  def checkUniqueName(tuple: (String, String)): Boolean = {
+    Robot.unique.get(tuple._1) match {
+      case Some(inner) => inner.get(tuple._2) match {
+        case Some(value) => value
+        case None =>
+          Robot.unique(tuple._1).put(tuple._2, true)
+          false
+      }
+      case None =>
+
+        Robot.unique.put(tuple._1, new mutable.HashMap[String, Boolean]())
+        Robot.unique.get(tuple._1).map(x => x.put(tuple._2, true))
+        false
+    }
+  }
+  def generateUniqueName: String = {
+    var tuple = generateName
+    while (checkUniqueName(tuple)) {
+      tuple = generateName
+    }
+    tuple._1 + tuple._2
+  }
+
   def reset(): Unit = {
-    name = generateName
+    name = generateUniqueName
   }
 }
 
+object Robot {
+  var unique = mutable.Map[String, mutable.Map[String, Boolean]]()
+}
