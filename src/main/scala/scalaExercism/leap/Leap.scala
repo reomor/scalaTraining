ca9b3da5b234c3bc6e5ec0255822666e6f6eefed
Reomor <reomor@gmail.com>
2019-02-22 23:07:16 +0300

    task004 improvements

--- src/main/scala/scalaExercism/leap/Leap.scala
+++ src/main/scala/scalaExercism/leap/Leap.scala
@@ -2,16 +2,6 @@ package scalaExercism.leap
 
 object Leap {
   def leapYear(year: Int): Boolean = {
-    if (year % 4 == 0) {
-      if (year % 400 == 0) {
-        true
-      } else if (year % 100 == 0) {
-        false
-      } else {
-        true
-      }
-    } else {
-      false
-    }
+    (year % 4 == 0) && ((year % 400 == 0) || !(year % 100 == 0))
   }
 }
