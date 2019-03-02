51eb26e32ef78f5c174c6a0ffdd6fc254af4047a
Reomor <reomor@gmail.com>
2019-03-02 23:00:26 +0300

    task006 refactoring

--- src/main/scala/scalaExercism/sieve/Sieve.scala
+++ src/main/scala/scalaExercism/sieve/Sieve.scala
@@ -7,15 +7,15 @@ object Sieve {
   type SieveElement = (Int, Boolean)
 
   def primes(limit: Int): List[Int] = {
-    val list = (2 to limit).map(x => new SieveElement(x, false)).toBuffer
+    val list = (2 to limit).map(x => new SieveElement(x, true)).toBuffer
     for(i <- 2 to limit) {
       for (j <- i - 2 + 1 to limit - 2) {
         if (list(j)._1 % i == 0) {
-          list(j) = new SieveElement(list(j)._1, true)
+          list(j) = new SieveElement(list(j)._1, false)
         }
       }
     }
-    list.filter(x => !x._2).map(x => x._1).toList
+    list.filter(x => x._2).map(x => x._1).toList
   }
 }
 
