59a0a08fbeb3e44de51b51d79f37c0e144f6b51e
reomor <reomor@gmail.com>
2019-03-06 15:26:15 +0300

    task010 refactoring

--- src/main/scala/scalaExercism/gradeSchool/School.scala
+++ src/main/scala/scalaExercism/gradeSchool/School.scala
@@ -2,28 +2,24 @@ package scalaExercism.gradeSchool
 
 class School {
   type DB = Map[Int, Seq[String]]
-  val internalDB = collection.mutable.HashMap.empty[Int, Seq[String]]
+  val internalDB = collection.mutable.SortedMap.empty[Int, Seq[String]]
 
   def add(name: String, g: Int): Unit = {
     internalDB.get(g) match {
       case Some(value) => internalDB.update(g, value :+ name)
       case None => internalDB += (g -> Seq(name))
     }
-    ()
   }
 
   def db: DB = {
-    internalDB.map(kv => (kv._1, kv._2)).toMap
+    internalDB.toMap
   }
 
   def grade(g: Int): Seq[String] = {
-    internalDB.get(g) match {
-      case Some(value) => value
-      case None => Seq()
-    }
+    internalDB.getOrElse(g, Seq())
   }
 
   def sorted: DB = {
-    internalDB.toList.sortWith(_._1 < _._1).toMap.map(kv => (kv._1, kv._2.sorted))
+    internalDB.toMap.map(kv => (kv._1, kv._2.sorted))
   }
 }
