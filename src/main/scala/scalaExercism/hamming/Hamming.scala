88b28b0987de38b28554a1599dbac681466c9e66
Reomor <reomor@gmail.com>
2019-03-23 19:34:07 +0300

    task019 tailrec to zip

--- src/main/scala/scalaExercism/hamming/Hamming.scala
+++ src/main/scala/scalaExercism/hamming/Hamming.scala
@@ -1,25 +1,16 @@
 package scalaExercism.hamming
 
-import scala.annotation.tailrec
-
 object Hamming {
   def distance(first: String, second: String): Option[Int] = {
-    @tailrec
-    def hammingDifference(index: Int, limit: Int, count: Int): Int = {
-      if (index >= limit) {
-        count
-      } else {
-        if (first.charAt(index) == second.charAt(index)) {
-          hammingDifference(index + 1, limit, count)
-        } else {
-          hammingDifference(index + 1, limit, count + 1)
-        }
+    def distance(first: String, second: String): Option[Int] = {
+      def hammingDifference(): Int = {
+        first.zip(second).count(pair => pair._1 != pair._2)
       }
-    }
-    if (first.length != second.length) {
-      None
-    } else {
-      Option(hammingDifference(0, first.length, 0))
+      if (first.length != second.length) {
+        None
+      } else {
+        Option(hammingDifference())
+      }
     }
   }
 }
